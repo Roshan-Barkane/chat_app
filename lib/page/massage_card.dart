6f900c1d6dbbdfba40e44e10a379a83d44b40e0d
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-07-21 19:37:20 +0530

    solve the massage overflow error

--- lib/page/massage_card.dart
+++ lib/page/massage_card.dart
@@ -26,26 +26,28 @@ class _MassageCardState extends State<MassageCard> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Container(
-          padding: EdgeInsets.all(mq.width * .04),
-          margin: EdgeInsets.symmetric(
-              horizontal: mq.width * .04, vertical: mq.height * .01),
-          decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 217, 237, 253),
-              borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                  bottomRight: Radius.circular(30)),
-              border: Border.all(color: Colors.blue.shade300)),
-          child: Text(
-            widget.message.msg +
-                "if context are long then come to error of message overflow and app is not seeble",
-            style: const TextStyle(fontSize: 15, color: Colors.black),
+        Flexible(
+          child: Container(
+            padding: EdgeInsets.all(mq.width * .04),
+            margin: EdgeInsets.symmetric(
+                horizontal: mq.width * .04, vertical: mq.height * .01),
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 217, 237, 253),
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                    bottomRight: Radius.circular(30)),
+                border: Border.all(color: Colors.blue.shade300)),
+            child: Text(
+              widget.message.msg +
+                  "if context are long then come to error of message overflow and app is not seeble",
+              style: const TextStyle(fontSize: 15, color: Colors.black),
+            ),
           ),
         ),
         // show the time massage
         Padding(
-          padding: EdgeInsets.only(right: mq.width * .04),
+          padding: EdgeInsets.only(right: mq.width * .04, left: mq.width * .05),
           child: Text(
             widget.message.send,
             style: const TextStyle(color: Colors.black54, fontSize: 14),

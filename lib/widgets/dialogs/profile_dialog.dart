993e151482cd2674e6aeed8355eda404e9a873cf
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-09-02 09:28:57 +0530

    complete the profile dialog  feature .

--- lib/widgets/dialogs/profile_dialog.dart
+++ lib/widgets/dialogs/profile_dialog.dart
@@ -54,6 +54,7 @@ class ProfileDialog extends StatelessWidget {
               child: SizedBox(
                 child: IconButton(
                   onPressed: () {
+                    Navigator.pop(context);
                     Navigator.push(
                         context,
                         MaterialPageRoute(
